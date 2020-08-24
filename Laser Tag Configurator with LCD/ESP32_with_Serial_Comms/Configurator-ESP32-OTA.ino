f8316e9094ba97adb6158578703cd941e7174265
Laser Tag Mods <51645114+LaserTagMods@users.noreply.github.com>
2020-08-24 08:35:42 -0700

    Update Configurator-ESP32-OTA.ino

--- Laser Tag Configurator with LCD/ESP32_with_Serial_Comms/Configurator-ESP32-OTA.ino
+++ Laser Tag Configurator with LCD/ESP32_with_Serial_Comms/Configurator-ESP32-OTA.ino
@@ -86,8 +86,8 @@ static BLEUUID    charRXUUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"); // CHANGE 
 static BLEUUID    charTXUUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"); // CHANGE ME !!!!
 int GunID = 0; // this is the gun or player ID, each esp32 needs a different one, set "0-63"
 // This is the WIFI info for OTA Update Mode
-const char* ssid = "maxipad";
-const char* password = "9165047812";
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
 //******************* IMPORTANT *********************
 //******************* IMPORTANT *********************
 //******************* IMPORTANT *********************
